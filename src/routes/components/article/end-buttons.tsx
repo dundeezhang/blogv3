6fd5b3039c777c8686c95fea5caf6b1120238344
dundeezhang <dundee_zhang@hotmail.com>
2025-08-30 23:29:46 -0400

    ux update

--- src/routes/components/article/end-buttons.tsx
+++ src/routes/components/article/end-buttons.tsx
@@ -2,16 +2,16 @@ export default function EndButtons() {
   return (
     <div className="end-buttons">
       <button
-        onClick={() => (window.location.href = "/")}
-        className="blur-bg button"
-      >
-        Back to Home
-      </button>
-      <button
         onClick={() => (window.location.href = "/articles")}
         className="blur-bg button"
       >
         Read other Articles
+      </button>
+      <button
+        onClick={() => (window.location.href = "/")}
+        className="blur-bg button"
+      >
+        Home Page
       </button>
     </div>
   );
